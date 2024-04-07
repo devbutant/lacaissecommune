a7692d40f5091af747971b51740988c65f635e48
emmanuelniasse <emmanuelniasse@gmail.com>
2024-04-07 19:44:06 +0200

    refactor: data set

--- db/lacaissecommune.sql
+++ db/lacaissecommune.sql
@@ -1,9 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
--- Hôte : localhost:8889
--- Généré le : jeu. 23 mars 2023 à 18:29
 -- Version du serveur : 5.7.34
 -- Version de PHP : 8.0.8
 
@@ -57,47 +54,6 @@ CREATE TABLE `contribute` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `contribute`
---
-
-INSERT INTO `contribute` (`id_pots`, `id_contributor`, `sum`, `comment`, `date`) VALUES
-(1, 16, 5, '', '2022-06-17 11:03:38'),
-(1, 19, 33.5, '', '2022-06-17 13:35:37'),
-(1, 49, 29, 'zrfebg', '2023-03-23 19:17:22'),
-(4, 17, 33.5, '', '2022-06-17 11:23:09'),
-(4, 18, 0, 'sfg', '2022-06-17 11:23:17'),
-(4, 21, 0, 'Je ne donne pas d\'argent, mais un simple commentaire', '2022-06-17 13:40:54'),
-(4, 22, 10, '', '2022-06-17 13:41:10'),
-(4, 23, 5, '', '2022-06-17 13:47:17'),
-(4, 24, 5, '', '2022-06-17 13:47:27'),
-(4, 25, 0, 'zeg', '2022-06-17 13:47:34'),
-(4, 26, 0, 'rzgeth', '2022-06-17 13:47:44'),
-(4, 27, 0, 'ok', '2022-06-17 13:52:51'),
-(4, 28, 0, 'g', '2022-06-17 13:55:30'),
-(4, 29, 0, 'g', '2022-06-17 13:55:34'),
-(4, 30, 0, 'rgetfh', '2022-06-17 13:56:03'),
-(4, 31, 0, 'rgetfh', '2022-06-17 13:56:43'),
-(4, 32, 0, 'rgetfh', '2022-06-17 13:56:44'),
-(4, 33, 0, 'rgetfh', '2022-06-17 13:56:49'),
-(4, 34, 0, 'rgetfh', '2022-06-17 13:57:36'),
-(4, 35, 0, 'rgetfh', '2022-06-17 13:58:21'),
-(4, 36, 0, 'rgetfh', '2022-06-17 13:58:34'),
-(4, 37, 0, 'rgetfh', '2022-06-17 13:58:51'),
-(4, 38, 0, 'rgetfh', '2022-06-17 13:59:02'),
-(4, 39, 0, 'rr', '2022-06-17 14:13:36'),
-(4, 40, 0, 'r', '2022-06-17 14:13:43'),
-(4, 41, 5, '', '2022-06-17 14:13:51'),
-(4, 42, 0, 'efzr', '2022-06-17 14:16:01'),
-(4, 43, 0, 'efrgz', '2022-06-17 14:21:11'),
-(4, 44, 0, 'efeef', '2022-06-17 14:21:33'),
-(4, 45, 0, 'Test', '2022-06-17 14:22:44'),
-(4, 46, 0, 'fezr', '2022-06-17 14:25:39'),
-(4, 47, 0, 'grz', '2022-06-17 14:26:33'),
-(4, 48, 33.5, 'rrr', '2022-06-17 14:27:36');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `contributors`
 --
 
@@ -107,46 +63,6 @@ CREATE TABLE `contributors` (
   `photo` varchar(255) NOT NULL DEFAULT 'logo-lcc.svg',
   `e_mail` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `contributors`
---
-
-INSERT INTO `contributors` (`id_contributor`, `name`, `photo`, `e_mail`) VALUES
-(16, 'Emmanuel - LCC', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(17, 'r', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(18, 'r', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(19, 'rr', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(20, 'Albert', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(21, 'Albert', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(22, 'Argent sans com', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(23, 'rr', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(24, 'RRRR', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(25, 'RR', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(26, 'zrgte', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(27, 'lea', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(28, 'r', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(29, 'r', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(30, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(31, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(32, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(33, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(34, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(35, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(36, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(37, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(38, 'rtegb', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(39, 'rr', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(40, 'r', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(41, '\'', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(42, 'ezrfg', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(43, 'ezrg', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(44, 'okok', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(45, 'Dupont', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(46, 'ezfr', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(47, 'frzg', 'logo-lcc.svg', 'def-une-adresse@gmail.fr'),
-(48, 'rrr', 'Capture d’écran 2022-06-15 à 20.21.53.png', 'def-une-adresse@gmail.fr'),
-(49, 'Dup', '1.jpg', 'def-une-adresse@gmail.fr');
 
 -- --------------------------------------------------------
 
@@ -178,8 +94,6 @@ CREATE TABLE `pots` (
 INSERT INTO `pots` (`id_pots`, `id`, `slug`, `name`, `description`, `photo`, `sum_limit`, `recipient`, `organizer`, `date_start`, `date_end`, `public`, `my_pot`, `total`) VALUES
 (1, 2, 'soutenir-lacaissecommune', 'Soutenir LCC', 'Cette caisse a pour but de soutenir le projet LaCaisseCommune', 'logo-lcc.svg', 100000, 'LaCaisseCommune', 'LaCaisseCommune', '2022-06-12', '2030-01-12', 1, 1, 51517.9),
 (3, 2, 'fabrice-', 'Fabrice', 'Anniversaire de Fab', 'boite-cadeau.png', 5000, 'Fab', 'Manu', '2022-06-13', '2022-06-30', 0, 1, 33.4),
-(4, 2, 'ezgrte', 'test', 'test', 'Capture d’écran 2022-06-15 à 20.25.11.png', 2000, 'fzrg', 'efzrgt', '2022-06-17', '2022-06-17', 1, 1, 125),
-(5, 2, 'fesgrdtgnfh', 'ezrget', 'zrge', 'Capture d’écran 2022-06-15 à 20.25.11.png', 500, 'fsrgd', 'sgrd', '2022-06-17', '2022-06-17', 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -277,11 +191,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `fname`, `name`, `photo`, `email`, `pass`, `phone`, `dob`) VALUES
 (6, 'Emmanuel', 'Niasse', 'user.jpg', 'emmanuelniasse@gmail.com', '$2y$10$hAxH7OsKLMnZLzC8zkgFbuZSAmQu9YdvaPjlfTTA3aPS2if7gEXCC', '0782464332', '1996-10-01'),
-(7, 'Myriam', 'O', 'user.jpg', 'myriam.ou@gmail.com', '$2y$10$YsJL5K8.uTXOL/FtDkyNfe8K.DwngFQzzrrbbPvMhz7EKw5.PFVK6', '0782464222', '1998-06-08'),
 (12, 'Eliott', 'Ricardo', 'boite-cadeau.png', 'ricardo@gmail.com', '$2y$10$VD.7XqIfJGVFjxIsgV/co.sh0t1bb7cmCYSOLp0Y1qQgV2DimFpaO', '093939399', '2014-02-27'),
 (13, 'Alain', 'Robert', 'alain-robert.jpg', 'alain@yahoo.com', '$2y$10$FUe1aFTboxoI0UwU6YC3se0o1Httt5VaE/DCkgXLKfn2elrOEGsR.', '0782464222', '1989-02-06'),
-(14, 'rte', 'Ok', 'Capture d’écran 2022-06-15 à 20.25.11.png', 'test@yahoo.com', '$2y$10$2QsrTfndHAgTupc0U6DLD.LtVkIvVammb9pZg6wHLsa.RJplF1NXa', '0600000000', '1990-06-17'),
-(15, 'Test', 'Essaie', NULL, 'pk@yahoo.com', '$2y$10$hKeQo8OVp0Erj7/guppxfONS0II0YQ7omFEHX07lB3aSWzVd4qQi6', '0929292929', '2000-06-18');
 
 --
 -- Index pour les tables déchargées
